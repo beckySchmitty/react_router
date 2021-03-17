13a61273cd8e234c3ce06949450d1f0099ba3871
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-03-16 22:23:42 -0400

    inital notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Home from "./Home";
+import Eat from "./Eat";
+import Drink from "./Drink";
+import NavBar from "./NavBar";
+
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path="/drink">
+          <Drink />
+        </Route>
+        <Route exact path="/eat">
+          <Eat />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
